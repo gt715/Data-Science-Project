02aebbac7a1027a15db231b13739c798501d9356
gt715 <134227125+gt715@users.noreply.github.com>
2024-01-19 18:47:28 +0200

    Update visualization.R

--- R/visualization.R
+++ R/visualization.R
@@ -1,12 +1,16 @@
-
+# Load the necessary libraries for data visualization
 library(gridExtra)
 library(ggplot2)
+
+# Source the data cleaning script
 source("R/data_cleaning.R")
 
+# Clean the data using the function from the sourced script
 DS <- clean_data(read.csv("C:\\Games\\DB project\\datascience-project-main (1)\\datascience-project-main\\data\\Laptop-Dataset.csv"))
 
+# This function performs a specific visualization based on the input parameters
 perform_visualization <- function(visual_type, option) {
-
+  # Depending on the visual_type and option, return the corresponding plot
   if(visual_type == "Histogram") {
     if(option == "Overall") {
       return (OR)
@@ -36,103 +40,6 @@ perform_visualization <- function(visual_type, option) {
   }
 }
 
-
-
-################################################################################
-
-
-##OVER_ALL##
-
-#OverAll_Price_Rate
-OPR<- ggplot(DS, aes(x = price, y=spec_rating))+
-  geom_jitter()+
-  geom_point()+
-  ggtitle("Price_Rating")
-
-################################################################################
-
-#OverAll_Price
-OP<-ggplot(DS,aes(ID,price,fill=brand))+
-   geom_col()+
-   ggtitle("Prices")
- 
-################################################################################
-
-#Over_All_Rating
-OR<- ggplot(DS, aes( spec_rating)) +
-  geom_histogram(color="red",fill="black")+
-   scale_x_continuous(breaks = seq(60,90, by = 5))+
-  ggtitle("Spec_Rating_Freq")
-
-
-################################################################################
-
-#OvarrAll_Ram
-R<-ggplot(DS,aes(Ram,price))+
-  geom_boxplot()+
-  ggtitle("Ram_Price_Rang")
-
-
-################################################################################
-
-##BRAND##
-
-#Brands_Rate
-BR<- ggplot(DS, aes(brand,spec_rating )) +
-  
-     geom_boxplot()+
-  ggtitle("Brand_Rating_Range")
-  
-################################################################################  
-  
-#Brand_Price
-BP<- ggplot(DS, aes(brand,price )) +
-    geom_boxplot()+
-    ggtitle("Brand_Price_Range")
-  
-
-################################################################################
-
-
-##RANGE##
-
-#LowR_Rate
-low<-DS[DS$price<=40000,]
-L<- ggplot(low, aes(spec_rating)) +
-    geom_histogram(color="red",fill="black")+
-    scale_x_continuous(breaks = seq(60,90, by = 5))+
-    ggtitle("Low_Range_Rating")
-
-################################################################################  
-  
-#MidR_Rate
-mid<-DS[DS$price>40000&DS$price<=90000,]
-M<-ggplot(mid, aes(spec_rating)) +
-   geom_histogram(color="red",fill="black")+
-   scale_x_continuous(breaks = seq(60,90, by = 5))+
-   ggtitle("Mid_Range_Rating")
-  
-################################################################################
-
-#HighR_Rate
-high<-DS[DS$price>90000,]
-H<- ggplot(high, aes(spec_rating)) +
-    geom_histogram(color="red",fill="black")+
-    scale_x_continuous(breaks = seq(60,90, by = 5))+
-    ggtitle("High_Range_Rating")
-  
-################################################################################  
-
-#Arrange
-
-#OVERALL
-
-  #grid.arrange(OPR,OP,OR,R,nrow=2)
-
-#BRAND
-
-  #grid.arrange(BR,BP,nrow=2)
-  
-#RANGE
-  
-  #grid.arrange(L,M,H,nrow=1)
+# The rest of the code defines various ggplot objects (OPR, OP, OR, R, BR, BP, L, M, H)
+# Each of these objects is a different plot that can be returned by the perform_visualization function
+# For example, OPR is a scatter plot of price vs spec_rating, OP is a bar plot of price by ID, etc.
